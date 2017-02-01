201987531d7f87a5b5c8e929190db81e71ad0378
AlexTheSunbeam <aliaksandr.tachko@gmail.com>
2017-02-01 23:37:05 +0300

    fsm : first implementation

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,50 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+//!add throw Error; 	
+    	this.state = config.initial;
+    	this.states = config.states;
+    	this.history = [];
+    	this.step = 0;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+    	return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+    	this.state = state;
+    	this.history[this.step + 1] = state;
+    	this.step++;
+// cut this.step++ ?    	
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+    	this.state = this.states[this.state][transitions][event];
+    	this.history[this.step + 1] = this.state;
+    	this.step++;
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+    	this.state = this.history[0];
+// history reset?
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +54,40 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+// implement it    	
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+// check if there is prev step
+    	this.state = this.history[this.step - 1];
+    	this.step--;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+// check if there is next step
+    	this.state = this.history[this.step + 1];
+    	this.step++;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+//check it
+    clearHistory() {
+    	this.history = this.history[0];
+    	this.step = 0;
+    }
 }
 
 module.exports = FSM;
